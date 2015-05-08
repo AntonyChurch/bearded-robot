44c3afa7baed70d18d8634ffbfa428e05fa99c12
Antony Church <achurch44.ac@gmail.com>
2015-05-08 19:21:52 +0100

    Added wiringPi functionality to interface file

--- interface.c
+++ interface.c
@@ -1,14 +1,37 @@
+#include <wiringPi.h>
+
+//Used [WiringPi docs](http://wiringpi.com/wp-content/uploads/2013/03/pins.pdf)
+int pins[21];
+
 int setup()
 {
-  return 1;
+  int setupValue = wiringPiSetup();
+  return setupValue;
 }
 
 int turn_on(int port)
 {
-  return 12;
+  configure_port(port);
+
+  digitalWrite(port, 1);
+
+  return 1;
 }
 
 int turn_off(int port)
 {
-  return 123;
+  configure_port(port);
+
+  digitalWrite(port, 0);
+
+  return 1;
+}
+
+void configure_port(int port)
+{
+  if(pins[port] == null)
+  {
+    pinMode(port, OUTPUT);
+    pins[port] = port;
+  }
 }
